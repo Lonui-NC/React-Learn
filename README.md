ff1d48dca4dbeb5e0950641bd5f4d116cf6f6e16
Nigel Zhang <congpang1201@163.com>
2019-03-07 16:54:33 +0800

    Update with serveral new chapters

--- README.md
+++ README.md
@@ -16,3 +16,22 @@ March 6, 2019
 ** The tip is never to use Inheritance since Composition can fulfill all the needs and much more flexible.
 * React Guide - Thinking in React
 ** This one is really interesting https://reactjs.org/docs/thinking-in-react.html
+
+March 7, 2019
+* React Guide - Accessibility
+* React Guide - Code-Splitting
+** Bundling
+** Code-spliting - improve the load performance
+** import() => export => include/require other JS files
+** React.lazy => lazy load JS files
+** Suspense => handle if the external JS has not be loaded yet
+** Error Boundary => Add general error handling stuffs
+** Route-based code splitting => Named Exports
+* React Guide - Context 
+** Context - share the same attributes 
+* React Guide - Error Boundaries 
+** This is useful!
+
+
+
+
--- react-guide/src/index.js
+++ react-guide/src/index.js
@@ -558,10 +558,91 @@ class FilterableProductTable extends React.Component {
   }
 }
 
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null, 
+      errorInfo: null
+    };
+  }
 
+  componentDidCatch(error, errorInfo) {
+    this.setState({
+      error: error,
+      errorInfo: errorInfo
+    });
+  }
 
+  render() {
+    if (this.state.errorInfo) {
+      return (
+        <div>
+          <h2>Something is wrong!</h2>
+          <details style={{ whiteSpace: 'pre-wrap'}}>
+            {this.state.error && this.state.error.toString()}
+            <br />
+            {this.state.errorInfo.componentStack}
+          </details>
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
+
+class BuggyCounter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {counter: 0}
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(({counter}) => ({
+      counter: counter + 1
+    }))
+  }
+
+  render() {
+    if (this.state.counter === 5) {
+      throw new Error("I crashed!");
+    }
+    return <h1 onClick={this.handleClick}>{this.state.counter}</h1>
+  }
+}
+
+function ErrorApp() {
+  return (
+    <div>
+      <p>
+        <b>
+          This is an example of error boundaries in React 16.
+          <br /><br />
+          Click on the numbers to increase the counters.
+          <br />
+          The counter is programmed to throw when it reaches 5. This simulates a JavaScript error in a component.
+        </b>
+      </p>
+      <hr />
+      <ErrorBoundary>
+        <p>These two counters are inside the same error boundary. If one crashes, the error boundary will replace both of them.</p>
+        <BuggyCounter />
+        <BuggyCounter />
+      </ErrorBoundary>
+      <hr />
+      <p>These two counters are each inside of their own error boundary. So if one crashes, the other is not affected.</p>
+      <ErrorBoundary>
+        <BuggyCounter />
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <BuggyCounter />
+      </ErrorBoundary>
+    </div>
+  );
+}
 
 ReactDOM.render(
-  <FilterableProductTable products={PRODUCTS}/>,
+  <ErrorApp />,
   document.getElementById('root')
 );
